598353105803048aa8fc8bc2f02b25b5f1bfaf28
mateusz <mateusz.hladky@gmail.com>
2021-12-04 20:31:07 +0100

    Deleted channelId variable

--- commands/stfu.js
+++ commands/stfu.js
@@ -3,9 +3,6 @@ module.exports = {
     description: 'Mutuje kawę.',
     async execute(message, args, Discord, client) {
         const memberId = '561228400870686723';
-        const channelId = message.guild.channels.find(
-            channel => (channel.name = '📚┇gładkie korepetycje')
-        );
 
         message.channel.send('Kawa został zmutowany :)');
         message.guild.members.cache.get(memberId).voice.setMute(true);
