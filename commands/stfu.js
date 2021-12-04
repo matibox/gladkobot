3be5cb26a8a0e84f6b90b388fc5ed50621b4c349
mateusz <mateusz.hladky@gmail.com>
2021-12-04 20:41:47 +0100

    Added checking whether the user has the permissions to call the command. Added ability to mute or unmute the member.

--- commands/stfu.js
+++ commands/stfu.js
@@ -3,8 +3,23 @@ module.exports = {
     description: 'Mutuje kawę.',
     async execute(message, args, Discord, client) {
         const memberId = '561228400870686723';
+        const member = message.guild.members.cache.get(memberId);
 
-        message.channel.send('Kawa został zmutowany :)');
-        message.guild.members.cache.get(memberId).voice.setMute(true);
+        if (message.member.roles.find(role => role.name === 'Korepetytor')) {
+            let muted = member.voice.serverMute;
+            if (muted) {
+                member.voice.setMute(false);
+                message.channel.send('Kawa został odmutowany D:');
+            } else {
+                member.voice.setMute(true);
+                message.channel.send(
+                    'Kawa został zmutowany, wszyscy się cieszą!'
+                );
+            }
+        } else {
+            message.channel.send(
+                'Nie możesz zmutować/odmutować Kawy, beka z ciebie.'
+            );
+        }
     },
 };
